782b34b6eeed14d8d9e5a6c6091e155dc9b276b0
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-23 02:15:32 +0600

    payoo home page added through window.location

--- payoomfs/login.js
+++ payoomfs/login.js
@@ -3,8 +3,21 @@ document.getElementById('button-login').addEventListener('click', function(event
     // step - 2 prevent default behavior (prevent reloading browser)
     // vejal for beginners
     event.preventDefault();
-    console.log('login')
-     // step - 3 get the phone number
+    
+     // step - 3 get the phone number and the pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+
+    const pinNumber = document.getElementById('pin-number').value;
+    // console.log(phoneNumber, pinNumber);
+    // step - 4 validate phone and pin
+    // this is the not the ideal way
+    if(phoneNumber === '0175' && pinNumber === '1234'){
+        console.log('right')
+        window.location.href = './home.html';
+        // step - 5 allow user to use the website
+    }
+    else{
+        alert('wrong phone number or pin')
+    }
+
 })
