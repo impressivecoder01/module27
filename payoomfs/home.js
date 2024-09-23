c9844006e5e7401e2edfbdc971a74d6bcebbe811
impressivecoder01 <rohanrahman251@gmail.com>
2024-09-23 16:22:25 +0600

    got provided ammount and pin

--- payoomfs/home.js
+++ payoomfs/home.js
@@ -1,1 +1,12 @@
-// console.log('c')
+// add money to the account
+
+//step 1: add an event handler to the add money button inside the form
+document.getElementById("btn-add-money").addEventListener('click', function(event){
+    event.preventDefault();
+    // step 2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    // get the pin number provided
+    const pinNumberProvided = document.getElementById('input-pin-number').value;
+    console.log(pinNumberProvided)
+})
